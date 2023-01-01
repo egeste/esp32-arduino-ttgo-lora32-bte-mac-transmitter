306e34a8278fc73538d1693422b0b84c2794da29
Steve Regester <steve@studylog.com>
2023-01-01 14:47:32 -0800

    Add notes to transmitter.h

--- transmitter/transmitter.h
+++ transmitter/transmitter.h
@@ -1,5 +1,13 @@
 #pragma once
 
+/** Size comparisons
+Description         Size   Difference
+Baseline            286.6k          0
+Enable WiFi module  690.7k     404.1k // Need to slim this down
+Enable BLE module   980.6k     694.0k // Need to slim this down
+Enable GPS module   304.7k      18.0k
+*/
+
 #define TRACK_GPS
 // #define TRACK_BLE
 // #define TRACK_WIFI
